64544ade74ba55ca693e5f334df98f7e96d29406
reinaldoper <reinaldoper83@gmail.com>
2023-04-15 03:50:16 -0400

    feat: requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,24 @@
+from tech_news.database import (
+    find_news,
+)
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    response = find_news()
+    if len(response) == 0:
+        return []
+    else:
+        contagens = {}
+    for category in response:
+        if category["category"] in contagens:
+            contagens[category["category"]] += 1
+        else:
+            contagens[category["category"]] = 1
+
+    top_5 = sorted(contagens.items(), key=lambda x: (-x[1], x[0]))
+    top_5 = top_5[:5]
+    array = []
+    for contagem in top_5:
+        array.append(contagem[0])
+    return array
